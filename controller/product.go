d2eb968ffa7cf8b01f0e8291f37b1442a5d70d16
Wunkyyy <wun.warinda@gmail.com>
2023-03-07 17:47:04 +0700

    param and query

--- controller/product.go
+++ controller/product.go
@@ -9,11 +9,19 @@ import (
 type Product struct{}
 
 func (p Product) FindAll(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"FindAll": "OK"})
+	// /products?search=tu
+	search := ctx.Query("search")
+	categoryId := ctx.Query("categoryId")
+	ctx.JSON(http.StatusOK, gin.H{
+		"FindAll":    "OK",
+		"Search":     search,
+		"CategoryID": categoryId,
+	})
 }
 
 func (p Product) FindOne(ctx *gin.Context) {
-
+	id := ctx.Param("id")
+	ctx.JSON(http.StatusOK, gin.H{"ID": id})
 }
 
 func (p Product) Create(ctx *gin.Context) {
